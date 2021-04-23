69d391ed06b434994843ebb0d05268c386447627
Zeno Sewald <zsewa@outlook.de>
2021-04-23 09:58:01 +0200

    Restructure tests

--- test/wiesel/test_build.py
+++ test/wiesel/test_build.py
@@ -1,15 +1,19 @@
+import logging
 import os
+import random
+import string
 import tempfile
 from unittest import TestCase
 
+import wiesel
 from wiesel.wsl_distributions.manager import Dockerfile
 
 
 class TestDockerfile(TestCase):
     def test_build_tar_file(self):
         distro_from_dockerfile = Dockerfile(
-            dockerfile_path="wsl/docker/dockerfile_test_archlinux",
-            docker_context_path="./wsl/docker",
+            dockerfile_path="data/docker/dockerfile_test_archlinux",
+            docker_context_path="data/docker",
             distribution_name="built_with_docker",
             install_location="."
         )
@@ -21,7 +25,7 @@ class TestDockerfile(TestCase):
 
     def test_build_tar_file_without_given_context(self):
         distro_from_dockerfile = Dockerfile(
-            dockerfile_path="wsl/docker/dockerfile_test_archlinux",
+            dockerfile_path="data/docker/dockerfile_test_archlinux",
             docker_context_path=None,
             distribution_name="built_with_docker",
             install_location="."
@@ -34,8 +38,8 @@ class TestDockerfile(TestCase):
 
     def test_build_tar_file_default_name(self):
         distro_from_dockerfile = Dockerfile(
-            dockerfile_path="wsl/docker/dockerfile_test_archlinux",
-            docker_context_path="./wsl/docker",
+            dockerfile_path="data/docker/dockerfile_test_archlinux",
+            docker_context_path="data/docker",
             distribution_name="built_with_docker",
             install_location="."
         )
@@ -45,4 +49,17 @@ class TestDockerfile(TestCase):
         print(tar_file)
 
     def test_build(self):
-        pass
+        if wiesel.prerequisites.is_compatible_platform():  # only run this test on WSL compatible platforms
+            random_distro_name = ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
+
+            distro_from_dockerfile = Dockerfile(
+                dockerfile_path="data/docker/dockerfile_test_archlinux",
+                docker_context_path="data/docker",
+                distribution_name=random_distro_name,
+                install_location="."
+            )
+
+            distro = distro_from_dockerfile.build()
+            distro.unregister()
+        else:
+            logging.warning("Skipping incompatible testcase.")
