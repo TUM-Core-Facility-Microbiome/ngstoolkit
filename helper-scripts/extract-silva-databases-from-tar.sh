b47e5447ab7e35384f530ccb012bb4aa6e60b12b
Zeno Sewald <zsewa@outlook.de>
2021-07-02 10:10:10 +0200

    Make extraction target a variable

--- helper-scripts/extract-silva-databases-from-tar.sh
+++ helper-scripts/extract-silva-databases-from-tar.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 archive="$1"
+extract_to="$2"  # may often be wsl_distro_build/build-context/opt
 
 tmp_folder="/tmp/ngstoolkit-extract"
 mkdir "${tmp_folder}"
@@ -8,6 +9,6 @@ mkdir "${tmp_folder}"
 path_inside_tar="usr/local/databases/silva/build"
 tar -xvf "${archive}" -C "${tmp_folder}" "${path_inside_tar}"
 
-mv "${tmp_folder}"/"${path_inside_tar}"/* ../wsl_distro_build/build-context/opt
+mv "${tmp_folder}"/"${path_inside_tar}"/* "${extract_to}"
 
 rm -rf "${tmp_folder}"
