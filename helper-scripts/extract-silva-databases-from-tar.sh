38267ccccd5ddd48b6712ab98fdf7e6b05391ab5
Zeno Sewald <zsewa@outlook.de>
2021-07-02 09:39:22 +0200

    Add helper script to extract silva databases from tar archive

---
+++ helper-scripts/extract-silva-databases-from-tar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+archive="$1"
+
+tmp_folder="/tmp/ngstoolkit-extract"
+mkdir "${tmp_folder}"
+
+path_inside_tar="usr/local/bin/databases/silva"
+tar -xvf "${archive}" -C "${tmp_folder}" "${path_inside_tar}"
+
+mv "${tmp_folder}"/"${path_inside_tar}"/* ../wsl_distro_build/build-context/opt
+
+rm -rf "${tmp_folder}"
